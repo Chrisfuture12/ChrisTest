91fabe52bc397164dcfb48020c329967b851e5e3
Chris <chrismorrisrva@gmail.com>
2018-09-07 18:08:44 -0400

    This is our first commit

---
+++ Greetings.cs
@@ -0,0 +1,30 @@
+/*
+Greeting
+Names and a fun facts about each team member that you would like to share
+You need to submit:
+The message should contain:
+.cs file
+screenshot with the solution
+One submission per team with the names and contributions added into the submission note
+*/
+
+using System;
+using static System.Console;
+
+namespace assignmentOne
+{
+    class Greetings
+    {
+        static void Main()
+        {
+            WriteLine("Hafsa: Hello my name is Hafsa");
+            WriteLine("Chris: Hey my name is Chris");
+          WriteLine("Eunice: Hi my name is Eunice");
+           WriteLine("All: And we are the team savage 3");
+           WriteLine("Hafsa: A fun fact about me is that Red is my favorite color.");
+          WriteLine("Chris: A fun fact about me is that i love food.");
+          WriteLine("Eunice: A fun fact about me is that i have a twin brother.");
+        }
+    }
+}
+
